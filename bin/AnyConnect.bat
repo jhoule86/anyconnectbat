c97334f9bff58cc8152303db23213168873a880d
Jeff Houle <44010975+jhoule86@users.noreply.github.com>
2018-10-10 02:41:19 -0400

    add comment about how vpnui auto-force-close

--- bin/AnyConnect.bat
+++ bin/AnyConnect.bat
@@ -2,6 +2,7 @@
 :: AnyConnect.bat
 :: Turns the vpnagent service on or off. 
 :: After turning on, will attempt to start up vpnui from default location.
+:: (the vpnui executable force quits itself when the service is disabled)
 
 set serv=vpnagent
 set ui="C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\vpnui.exe"
